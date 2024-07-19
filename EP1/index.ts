73c2cba8da675746eee8410c0c35d052f804a893
JMLkewq <krittaphatklinchuen@gmail.com>
2024-07-19 16:14:07 +0700

    เปรียบเทียบค่า

--- EP1/index.ts
+++ EP1/index.ts
@@ -5,3 +5,9 @@ let ต้องการกลับบ้าน: boolean = true
 console.log (firstname)
 console.log (y)
 console.log (ต้องการกลับบ้าน)
+
+let a: number = 1200
+let b: number = 9
+console.log (a === b)
+console.log (a > b)
+console.log (a <= b)
