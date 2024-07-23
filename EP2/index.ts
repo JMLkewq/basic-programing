b677e282a7173eb2ae698a5a7e3cd0244f3e3d43
JMLkewq <krittaphatklinchuen@gmail.com>
2024-07-23 14:46:23 +0700

    function

--- EP2/index.ts
+++ EP2/index.ts
@@ -2,3 +2,13 @@ function add(a: number, b: number) {
     return a + b
 }
 console.log(add(7, 9))
+
+function lob(a: number, b: number) {
+    return a - b
+}
+console.log(lob(8, 4))
+
+function perm(a: number, b: number, c: number, d: number) {
+    return a * b * c * d
+}
+console.log(perm(10, 5, 55, 24))
