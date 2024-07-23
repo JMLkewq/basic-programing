7075572ea800e30128ce1539e9d2c5215b428ead
JMLkewq <krittaphatklinchuen@gmail.com>
2024-07-23 15:29:33 +0700

    function

--- EP2/index.ts
+++ EP2/index.ts
@@ -12,3 +12,13 @@ function perm(a: number, b: number, c: number, d: number) {
     return a * b * c * d
 }
 console.log(perm(10, 5, 55, 24))
+
+function grade(homework: number, midterm: number, final: number) {
+    let score: number = homework + midterm + final
+    if (score < 50) {
+        return 'สอบตก'
+    }else{
+        return 'สอบผ่าน'
+    }
+}
+console.log(grade(15, 16, 19));
