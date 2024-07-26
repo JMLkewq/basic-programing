d1dbbf3e82e60955d6e735548c3c0873bdaea79c
JMLkewq <krittaphatklinchuen@gmail.com>
2024-07-26 13:35:38 +0700

    function

---
+++ EP3/index.ts
@@ -0,0 +1,21 @@
+function hlloWorld() {
+   console.log("สวัสดี")
+}    
+
+'helloWorld()'
+
+
+function hlloName(Name: string) {
+    console.log("Kewq")
+ }    
+ 
+ 'helloName(Kewq)'
+
+ function getPi() {
+    return 3.14
+ }
+ 
+ getPi()
+
+ console.log('yes yes yes')
+ 
