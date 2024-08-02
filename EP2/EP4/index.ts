6dde391f0adbc08376f97c43270c1ff8357d438c
JMLkewq <krittaphatklinchuen@gmail.com>
2024-08-02 15:13:00 +0700

    function

---
+++ EP2/EP4/index.ts
@@ -0,0 +1,65 @@
+function day(dayOfWeek: number){
+    if (dayOfWeek === 1){
+        console.log('วันจัทร์')
+        
+        else if (dayOfWeek === 2)
+          console.log('วันอังคาร')
+    }
+    else if (dayOfWeek === 3)
+        console.log('วันพุธ')
+}
+else if (dayOfWeek === 4)
+    console.log('วันพฤหัสบดี'){
+}
+else if (dayOfWeek === 5)
+    console.log('วันศุกร์'){
+}
+else if (dayOfWeek === 6)
+    console.log('วันเสาร์'){
+}
+else if (dayOfWeek === 7)
+    console.log('วันอาทิตย์'){
+}
+
+day(4)
+
+
+
+
+function size(tell: string) {
+    switch(tell){
+        
+        case 's':
+        console.log ('ผอม')
+        break
+        case 'm':
+        console.log ('ผอม')
+        break
+        case 'l':
+        console.log ('ท้วม')
+        break
+        case 'xl':
+        console.log ('อ้วน')
+        break
+        case 'xl':
+        console.log ('อ้วน')
+        break
+    }
+}
+
+size("xl")
+
+function fixeddecimal(n: number, digit: number){
+    const x = n.toFixed(digit)
+    return x
+
+}
+console.log(fixeddecimal(45.60, 29))
+
+
+function fixeddecimal(n: number){
+    const siu = n.toLocaleString()
+    return siu
+
+}
+console.log(fixeddecimal(1000))
