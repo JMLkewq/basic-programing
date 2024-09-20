1d649b11df3439b80d61c9478208d87b45458608
JMLkewq <krittaphatklinchuen@gmail.com>
2024-09-20 16:36:43 +0700

    vde

--- ep10/index.ts
+++ ep10/index.ts
@@ -23,3 +23,35 @@ function register(email: string,password: string){
 }
 
 register('kew.com','8955485485')
+
+const database =[
+    {
+        email: 'user@gmail.com',
+        password: 'duck8888'
+    },
+    {
+        email: 'max@gmail.com',
+        password: 'foxc45465'
+    }
+]
+
+function decrypassword(password: string){
+    return password.replaceAll('+','u').replaceAll('*','6').replaceAll('^','0')
+    
+    }
+
+function login(email: string,password: string){
+    const user = database.filter(function(element, index){
+          return element.email === email
+    })
+    if (user.length > 0 ) {
+        if (user[0].password === password){
+            alert('เข้าสู่ระบบสำเร็จ ยินดีต้อนรับ')
+        }else{
+            alert('ไม่ถูกต้อง โปรดตรวจสอบ')
+        }
+    }else{
+        alert('ไม่พบผู้ใช้งาน')
+    }
+}
+login('dekd@gmail.com', '58484dekd')
