84d6cf2943145d38298871d0658afff2eadc764f
JMLkewq <krittaphatklinchuen@gmail.com>
2024-09-27 16:12:05 +0700

    test

---
+++ ep11/index.ts
@@ -0,0 +1,24 @@
+// const database =[
+//     {
+//         email: 'user@gmail.com',
+//         password: 'ZHVjazg4ODg='
+//     },
+    
+// ]
+
+// function login(email: string,password: string){
+//     const user = database.filter(function(element, index){
+//           return element.email === email
+//     })
+//     if (user.length > 0 ) {
+//         if (atob (user[0].password) === password){
+//             console.log ('เข้าสู่ระบบสำเร็จ ยินดีต้อนรับ')
+//         }else{
+//             console.log('ไม่ถูกต้อง โปรดตรวจสอบ')
+//         }
+//     }else{
+//         console.log('ไม่พบผู้ใช้งาน')
+//     }
+// }
+// login('user@gmail.com', 'duck8888')
+
